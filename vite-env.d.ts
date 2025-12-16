76b956c83ef0932547401476f7e41664b67dbcc5
jemhakdog <134377112+jemhakdog@users.noreply.github.com>
2025-12-16 16:55:38 +0800

    asd

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,9 @@
+// The reference to vite/client is removed to fix the type definition error.
+// We augment NodeJS.ProcessEnv to support the usage in the codebase without redeclaring process.
+
 declare namespace NodeJS {
   interface ProcessEnv {
     API_KEY: string;
+    [key: string]: string | undefined;
   }
 }
